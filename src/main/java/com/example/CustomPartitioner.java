bca917525b8feae7bb33972a1fec872e4f2c7212
하성록 <gktjdfhr2@naver.com>
2024-11-15 20:43:37 +0900

    ⚡️ "커스텀 파티션 사용"

---
+++ src/main/java/com/example/CustomPartitioner.java
@@ -0,0 +1,66 @@
+package com.example;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Partitioner;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.Cluster;
+import org.apache.kafka.common.InvalidRecordException;
+import org.apache.kafka.common.PartitionInfo;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.common.utils.Utils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+public class CustomPartitioner implements Partitioner {
+
+    private final static Logger logger = LoggerFactory.getLogger(SimpleProducer.class);
+    private final static String TOPIC_NAME = "test";
+    private final static String BOOTSTRAP_SERVERS = "my-kafka:9092";
+
+    public static void main(String[] args) {
+
+        Properties configs = new Properties();
+        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        configs.put(ProducerConfig.PARTITIONER_CLASS_CONFIG, CustomPartitioner.class);
+
+        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(configs);
+
+        // Pangyo 키가 담긴 경우 직접 구현한 partition을 통해 인덱스가 지정되는지 확인
+        ProducerRecord<String, String> record = new ProducerRecord<>("test", "Pangyo", "23");
+
+        kafkaProducer.send(record);
+        kafkaProducer.flush();
+        kafkaProducer.close();
+    }
+
+    @Override
+    public int partition(String topic, Object key, byte[] keyBytes, Object value, byte[] valueBytes, Cluster cluster) {
+        if (keyBytes == null) {
+            throw new InvalidRecordException("Need message key");
+        }
+        if (((String)key).equals("Pangyo")) {
+            return 0;
+        }
+
+        List<PartitionInfo> partitions = cluster.partitionsForTopic(topic);
+        int numPartitions = partitions.size();
+        return Utils.toPositive(Utils.murmur2(keyBytes)) % numPartitions;
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public void configure(Map<String, ?> configs) {
+
+    }
+}
