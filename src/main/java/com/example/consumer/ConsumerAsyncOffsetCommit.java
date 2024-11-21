6c64fbcd183bf0710b5911f396480e8eb11ea00c
하성록 <gktjdfhr2@naver.com>
2024-11-21 20:28:30 +0900

    ⚡️"컨수머 비동기 커밋 샘플코드"

---
+++ src/main/java/com/example/consumer/ConsumerAsyncOffsetCommit.java
@@ -0,0 +1,48 @@
+package com.example.consumer;
+
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+public class ConsumerAsyncOffsetCommit {
+    private final static Logger logger = LoggerFactory.getLogger(SimpleConsumer.class);
+    private final static String BOOTSTRAP_SERVERS = "my-kafka:9092";
+    private final static String GROUP_ID = "test-group";
+    private final static String TOPIC_NAME = "test";
+
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
+        // KafkaProducer에서 직렬화한 방식과 동일한 타입의 역직렬화를 사용해야 한다.
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+        KafkaConsumer kafkaConsumer = new KafkaConsumer(properties);
+        // 컨슈머 그룹을 통해 목적에 맞게 동일한 역할을 하는 컨슈머를 묶어 관리한다.
+        kafkaConsumer.subscribe(Arrays.asList(TOPIC_NAME));
+
+        while (true) {
+            ConsumerRecords<String, String> records = kafkaConsumer.poll(Duration.ofSeconds(1));
+            Map<TopicPartition, OffsetAndMetadata> currentOffset = new HashMap<>();
+
+            for (ConsumerRecord<String, String> record : records) {
+                logger.info("{}", record);
+                currentOffset.put(
+                        new TopicPartition(record.topic(), record.partition()),
+                        new OffsetAndMetadata(record.offset() + 1, null)
+                        );
+                kafkaConsumer.commitSync();
+            }
+        }
+    }
+
+}
